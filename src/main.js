e558e98e4dfe16b7db317288a35b92223957a98e
bbalkon <bbalkonsky@gmail.com>
2019-11-03 23:12:41 +0500

    old queries

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ const app = express();
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
-let responseResult;
 
 let db = new sqlite3.Database('src/log-base.db', sqlite3.OPEN_READONLY, (err) => {
     if (err) {
@@ -11,8 +10,6 @@ let db = new sqlite3.Database('src/log-base.db', sqlite3.OPEN_READONLY, (err) =>
     }
     console.log('Connected to the database.');
 });
-
-
 
 // db.close((err) => {
 //     if (err) {
@@ -23,53 +20,75 @@ let db = new sqlite3.Database('src/log-base.db', sqlite3.OPEN_READONLY, (err) =>
 
 app.use(cors());
 
+// app.use(express.static(__dirname + "/dist"));
+
 app.get("/user/:userId", function(request, response){
     const sql = `SELECT * FROM logs WHERE user_id = ?`;
     const userId = request.params["userId"];
 
     db.serialize(() => {
-        db.all(sql, userId, (err, response) => {
+        db.all(sql, userId, (err, result) => {
             if (err) {
                 console.error(err.message);
             }
-            responseResult = response;
+            response.json(result);
         });
     });
-    response.json(responseResult);
 });
 
 app.get("/day/:dayDate", function(request, response){
-    const sql = 'SELECT * FROM logs WHERE time < :today AND time > :yesterday';
-    response.json(request.params["dayDate"]);
-});
-
-app.get("/period/:startDate", function(request, response){
-    const sql = 'SELECT * FROM logs WHERE time > ?';
-    const startDate = request.params["startDate"];
+    const sql = 'SELECT * FROM logs WHERE date(time) = ?';
+    const parseDate = request.params["dayDate"];
 
     db.serialize(() => {
-        db.all(sql, startDate, (err, response) => {
+        db.all(sql, parseDate, (err, result) => {
             if (err) {
                 console.error(err.message);
             }
-            responseResult = response;
+            response.json(result);
         });
     });
-    response.json(responseResult);
+});
+
+app.get("/period/:period", function(request, response){
+    const sql = 'SELECT * FROM logs WHERE date(time) > DATE("now", ?)';
+    let selectedPeriod = '';
+    switch (request.params["period"]) {
+        case 'week':
+            selectedPeriod = '-7 day';
+            break;
+        case 'month':
+            selectedPeriod = '-1 month';
+            break;
+        case 'year':
+            selectedPeriod = '-1 year';
+            break;
+        default:
+            response.redirect("/");
+            return;
+    }
+
+    db.serialize(() => {
+        db.all(sql, selectedPeriod, (err, result) => {
+            if (err) {
+                console.error(err.message);
+            }
+            response.json(result);
+        });
+    });
 });
 
 app.get("/", function(request, response){
     const sql = `SELECT * FROM logs`;
 
     db.serialize(() => {
-        db.all(sql, (err, response) => {
+        db.all(sql, (err, result) => {
             if (err) {
                 console.error(err.message);
             }
-            responseResult = response;
+            response.json(result);
         });
     });
-    response.json(responseResult);
 });
 
 app.listen(3000);
